9acc63efdcf3eedb79c1c20b9aab447b1149c7c1
stevekanger <hi.stevekanger@gmail.com>
2024-05-08 20:12:00 -0500

    Removed unneccesary nonce field in settings page

--- src/templates/admin/page-settings.php
+++ src/templates/admin/page-settings.php
@@ -8,7 +8,6 @@ defined('ABSPATH') || exit; ?>
 <div class="wrap">
     <h1><?php echo PLUGIN_NAME; ?></h1>
     <form method='POST' action="options.php">
-        <?php wp_nonce_field(PLUGIN_PREFIX . '_settings'); ?>
         <?php
         settings_fields(PLUGIN_PREFIX);
         do_settings_sections(PLUGIN_PREFIX);
